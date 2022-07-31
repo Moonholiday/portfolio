3e60cc699b3ae2eacc05093ba3acf545b778f9a3
moonholiday <727moonholiday@gmail.com>
2022-07-31 12:36:30 +0545

    initial boilerplate

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/styles.css'
+
+function Footer () {
+  return (
+    <div className="footer" data-scroll-section>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">More</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer;
