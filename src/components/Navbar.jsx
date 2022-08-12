ac13886e40e3992cda0c916435f8363cbc6c8188
moonholiday <727moonholiday@gmail.com>
2022-08-12 20:57:19 +0545

    href addition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import '../styles/styles.css'
 import logo from '../sakshyam.png'
+import github from '../github.png'
+import linkedin from '../linkedin.png'
 import { Link } from 'react-router-dom'
 
 function Navbar () {
   return (
     <nav className="navbar" data-scroll-section>
-      <h1 className="logo"><Link to="/"><img src={logo} alt='Sakshyam'/></Link></h1>
-      <ul>
+      <Link to="/" className='logo'><img src={logo} alt='Sakshyam'/></Link>
+      <ul className='left'>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/more">More</Link></li>
+      </ul>
+      <ul className='right'>
+        <li><a href="www.google.com"><img src={github} alt='github'/></a></li>
+        <li><Link to="/contact"><img src={linkedin} alt='linkedin'/></Link></li>
       </ul>
     </nav>
   )
